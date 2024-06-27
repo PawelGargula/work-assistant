433330a4d89b9646a97e5db900a4024a4db02f9c
Paweł Gargula <pawel.gargula@interia.pl>
2024-06-27 21:25:51 +0200

    Start implementing

---
+++ src/app/ui/dashboard/pomodoro-timer.tsx
@@ -0,0 +1,5 @@
+export default function PomodoroTimer() {
+    return (
+        <div className="bg-gray-50 flex font-medium gap-2 h-[48px] items-center justify-center md:flex-none md:justify-start md:p-2 md:px-3 p-3 rounded-md text-sm w-full">Pomodoro Timer</div>
+    )
+}
--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -1,8 +1,8 @@
-import Link from 'next/link';
 import NavLinks from '@/src/app/ui/dashboard/nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
 import WorkAssistantLogo from '@/src/app/ui/work-assistant-logo';
+import PomodoroTimer from '@/src/app/ui/dashboard/pomodoro-timer';
 
 export default function SideNav() {
   return (
@@ -12,7 +12,8 @@ export default function SideNav() {
       >
         <WorkAssistantLogo />
       </div>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <div className="flex grow flex-row flex-wrap justify-between gap-2 md:flex-col">
+        <PomodoroTimer />
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form
