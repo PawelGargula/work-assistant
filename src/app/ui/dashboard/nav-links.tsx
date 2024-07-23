d7fbd7309e693fc08fa3cafb9b46de525cb0d408
Paweł Gargula <pawel.gargula@interia.pl>
2024-07-23 10:24:38 +0200

    Disable link if it's already current path

--- src/app/ui/dashboard/nav-links.tsx
+++ src/app/ui/dashboard/nav-links.tsx
@@ -8,10 +8,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
@@ -38,19 +35,22 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <Link
+          pathname === link.href
+          ? <span
             key={link.name}
-            href={link.href}
-            className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium focus-visible:outline-violet-500 hover:text-violet-600 hover:underline md:flex-none md:justify-start md:p-2 md:px-3",
-              {
-                'bgc-violet-100 text-violet-600': pathname === link.href
-              },
-            )}
+            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium focus-visible:outline-violet-500 md:flex-none md:justify-start md:p-2 md:px-3 bgc-violet-100 text-violet-600"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </Link>
+          </span>
+          : <Link
+          key={link.name}
+          href={link.href}
+          className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium focus-visible:outline-violet-500 hover:text-violet-600 hover:underline md:flex-none md:justify-start md:p-2 md:px-3"
+        >
+          <LinkIcon className="w-6" />
+          <p className="hidden md:block">{link.name}</p>
+        </Link>
         );
       })}
     </>
