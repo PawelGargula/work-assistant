dadaf6f8a7f1ada16a8f26792074052f41eeb066
Paweł Gargula <pawel.gargula@interia.pl>
2024-10-22 18:57:43 +0200

    Style table and its summary

--- src/app/ui/reports/report-by-day.tsx
+++ src/app/ui/reports/report-by-day.tsx
@@ -5,7 +5,7 @@ import { formatTimeDuration, getTimeDuration, getTimeTracksByDateRange } from "@
 
 export default function ReportByDay({ timeTracks, from, to }: { timeTracks: TimeTrack[], from: string | undefined, to: string | undefined }) {
     if (!from || !to) {
-        return <div>No date range provided</div>
+        return <div>No period selected</div>
     }
     
     const timeTracksByDay = getTimeTracksByDateRange(timeTracks, new Date(from), new Date(to));
@@ -16,39 +16,41 @@ export default function ReportByDay({ timeTracks, from, to }: { timeTracks: Time
     return (
         <>
             {timeTracksByDay.length > 0 ? 
-            <div className="overflow-auto">
-                <table className="min-w-full text-gray-900 md:table">
-                    <thead className="rounded-t-lg text-left text-sm font-normal">
-                    <tr>
-                        <th scope="col" className="px-4 py-1 font-medium sm:pl-6">
-                            Day
-                        </th>
-                        <th scope="col" className="px-3 py-1 font-medium">
-                            Tracked Time
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody className="bg-white">
-                    {timeTracksByDay?.map((timeTrack) => (
-                        <tr
-                        key={timeTrack.day}
-                        className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg"
-                        >
-                            <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                                {timeTrack.day}
-                            </td>
-                            <td className="whitespace-nowrap px-3 py-3">
-                                {formatTimeDuration(timeTrack.duration)}
-                            </td>
+            <>
+                <div className="max-h-[calc(100dvh-0.75rem-3rem-32px-2rem-39px-1.5rem-1.5rem-0.25rem-44px-1.5rem-3rem-0.75rem)] overflow-auto">
+                    <table className="min-w-full text-gray-900 md:table">
+                        <thead className="bg-gray-50 font-normal rounded-t-lg shadow sticky text-left text-sm top-0">
+                        <tr>
+                            <th scope="col" className="px-4 py-1 font-medium sm:pl-6">
+                                Day
+                            </th>
+                            <th scope="col" className="px-3 py-1 font-medium">
+                                Tracked Time
+                            </th>
                         </tr>
-                    ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody className="bg-white">
+                        {timeTracksByDay?.map((timeTrack) => (
+                            <tr
+                            key={timeTrack.day}
+                            className="w-full border-b py-3 text-sm last-of-type:border-none"
+                            >
+                                <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                                    {timeTrack.day}
+                                </td>
+                                <td className="whitespace-nowrap px-3 py-3">
+                                    {formatTimeDuration(timeTrack.duration)}
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div> 
                 <div className="bg-white flex mt-1 px-8 py-3 rounded-b-lg text-sm">
                     <span className="font-medium">Avarage:</span><span className='ml-1'>{formatTimeDuration(avaragePerDay)}</span>
                     <span className="font-medium ml-auto">Total:</span><span className='ml-1'>{formatTimeDuration(sumOfAllDays)}</span>
                 </div>
-            </div> 
+            </>
             : <span className="font-medium text-sm">There are currently no time tracks</span>}
         </>
     )
