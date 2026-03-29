8e8572c952f96a11e2f698ff6a456a4d5f38d456
Paweł Gargula <pawel.gargula@interia.pl>
2026-03-29 15:22:48 +0200

    Refactor googleAuthenticate function to remove error handling
    - Simplified the googleAuthenticate function by removing the try-catch block, allowing for direct sign-in without error handling.
    - This change streamlines the authentication process for Google sign-in.

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -25,14 +25,7 @@ export async function authenticate(
 }
 
 export async function googleAuthenticate() {
-  try {
-    await signIn("google", { redirectTo: "/dashboard" });
-  } catch (error) {
-    if (error instanceof AuthError) {
-      return 'Something went wrong.';
-    }
-    throw error;
-  }
+  await signIn("google", { redirectTo: "/dashboard" });
 }
 
 // Tasks
